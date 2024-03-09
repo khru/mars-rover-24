d6b800d89638ba0bbfcff163cb6fe2499fd877f7
Alberto Morales <kodenix@gmail.com>
2024-03-09 16:31:16 +0100

    refactor: renamed tests

--- typescript/tests/test.spec.ts
+++ typescript/tests/test.spec.ts
@@ -8,35 +8,35 @@ describe('Rovers should', () => {
     })
 
 
-    it('be facing east when it rotates right', () => {
+    it('be facing east when it rotates right and is facing north', () => {
         expect(rover.command('R')).toBe('0:0:E')
     })
 
-    it('be facing south when it rotates two times right', () => {
+    it('be facing south when it rotates right and is facing east', () => {
         expect(rover.command('RR')).toBe('0:0:S')
     })
 
-    it('be facing west when it rotates three times right', () => {
+    it('be facing west when it rotates right and is facing south', () => {
         expect(rover.command('RRR')).toBe('0:0:W')
     })
 
-    it('be facing north when it rotates four times right', () => {
+    it('be facing north when it rotates right and is facing west', () => {
         expect(rover.command('RRRR')).toBe('0:0:N')
     })
 
-    it('be facing west when it rotates left', () => {
+    it('be facing west when it rotates left and is facing north', () => {
         expect(rover.command('L')).toBe('0:0:W')
     })
 
-    it('be facing south when it rotates left', () => {
+    it('be facing south when it rotates left and is facing west', () => {
         expect(rover.command('LL')).toBe('0:0:S')
     })
 
-    it('be facing east when it rotates left', () => {
+    it('be facing east when it rotates left and is facing south', () => {
         expect(rover.command('LLL')).toBe('0:0:E')
     })
 
-    it('be facing north when it rotates left', () => {
+    it('be facing north when it rotates left and is facing east', () => {
         expect(rover.command('LLLL')).toBe('0:0:N')
     })
 })
