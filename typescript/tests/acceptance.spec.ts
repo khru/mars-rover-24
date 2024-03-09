49fe0823d63295b752adda0303bec2d21a53ed9f
Alberto Morales <kodenix@gmail.com>
2024-03-09 19:02:38 +0100

    Fixed acceptance test with correct command in the act

--- typescript/tests/acceptance.spec.ts
+++ typescript/tests/acceptance.spec.ts
@@ -4,6 +4,6 @@ describe('Testing Rovers positions', () => {
     it('Given command MMRMMLM rovers is in 2:3:N', () => {
         const rover = new Rover()
 
-        expect(rover.command('R')).toBe('2:3:N')
+        expect(rover.command('MMRMMLM')).toBe('2:3:N')
     })
 })
