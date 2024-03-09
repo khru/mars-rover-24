c44e953a48499555b987da50fd8f5d2fea99482f
Alberto Morales <kodenix@gmail.com>
2024-03-09 16:27:02 +0100

    refactor-test: extract rover build to beforeach function

--- typescript/tests/test.spec.ts
+++ typescript/tests/test.spec.ts
@@ -1,47 +1,42 @@
 import { Rover } from '../src/Rover'
 
 describe('Rovers should', () => {
-    it('be facing east when it rotates right', () => {
-        const rover = new Rover()
+    let rover: Rover;
 
+    beforeEach(()=> {
+        rover = new Rover()
+    })
+
+
+    it('be facing east when it rotates right', () => {
         expect(rover.command('R')).toBe('0:0:E')
     })
 
     it('be facing south when it rotates two times right', () => {
-        const rover = new Rover()
-
         expect(rover.command('RR')).toBe('0:0:S')
     })
 
     it('be facing west when it rotates three times right', () => {
-        const rover = new Rover()
-
         expect(rover.command('RRR')).toBe('0:0:W')
     })
 
     it('be facing north when it rotates four times right', () => {
-        const rover = new Rover()
-
         expect(rover.command('RRRR')).toBe('0:0:N')
     })
 
     it('be facing west when it rotates left', () => {
-        const rover = new Rover()
         expect(rover.command('L')).toBe('0:0:W')
     })
 
     it('be facing south when it rotates left', () => {
-        const rover = new Rover()
         expect(rover.command('LL')).toBe('0:0:S')
     })
 
     it('be facing east when it rotates left', () => {
-        const rover = new Rover()
         expect(rover.command('LLL')).toBe('0:0:E')
     })
 
     it('be facing north when it rotates left', () => {
-        const rover = new Rover()
         expect(rover.command('LLLL')).toBe('0:0:N')
     })
 })
